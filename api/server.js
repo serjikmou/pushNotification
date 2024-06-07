e69eef2f2503fe5f88601910c4685c192f2100b3
Sajjad Mousavi <sm@qaap.oi>
2024-06-07 23:37:40 +0330

    edit

--- api/server.js
+++ api/server.js
@@ -11,26 +11,26 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "client")));
 
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
 
-  // Request headers you wish to allow
-  res.setHeader("Access-Control-Allow-Headers", "*");
+//   // Request headers you wish to allow
+//   res.setHeader("Access-Control-Allow-Headers", "*");
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 const publicVapidKey =
   "BPPsaQD6XUBcleOncy3YYBQMhrTxYc88kKb8nuvB8K1K5wtWIBIjC2YtXrN4eOyhmY_V2cjlIiRTP79X8dA7pfY"; // REPLACE_WITH_YOUR_KEY
